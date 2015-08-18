d26134cbf49a51b610b6308d115c5f8aa33f4aa6
Thomas Shields <thomas.shields@gatech.edu>
2015-08-18 05:22:56 -0400

    Updated app path and gitignore

--- vcs/wsgi.py
+++ vcs/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/opt/python/current/app')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcs.settings")
 
 from django.core.wsgi import get_wsgi_application
